de90a342d6c69f966071b39b79bc5a3d55e0ab30
bexruz <bexruzbahronov2000@gmail.com>
2022-02-04 04:02:43 +0500

    design bookInfo

--- src/views/book.vue
+++ src/views/book.vue
@@ -2,10 +2,35 @@
   <div>
     <v-container>
       <v-row>
-        <v-col cols="12"
-          ><p>
-            {{ bookInfo }}
-          </p>
+        <v-col cols="12" class="text-center" v-if="!bookInfo">
+          <v-progress-circular
+            :size="100"
+            color="primary"
+            indeterminate
+          ></v-progress-circular>
+        </v-col>
+        <v-col v-else cols="12" class="d-flex">
+          <span class="imgPosition mx-5">
+            <img
+              class="imgBorder"
+              :src="imgLinks(bookInfo.volumeInfo)"
+              alt=""
+            />
+            <p class="textPosition">#book</p>
+          </span>
+          <span class="d-block">
+            <span v-html="description(bookInfo.volumeInfo)"></span>
+
+            <span class="d-flex py-3">
+              <p>Author:</p>
+              <span
+                v-for="author in authors(bookInfo.volumeInfo)"
+                :key="author.index"
+              >
+                <p class="px-2 primary--text">{{ author }};</p>
+              </span></span
+            >
+          </span>
         </v-col>
       </v-row>
     </v-container>
@@ -27,8 +52,34 @@ export default {
   },
   methods: {
     ...mapActions("bookInfo", ["get_bookInfo"]),
+    imgLinks(img) {
+      return img?.imageLinks.small || "";
+    },
+    description(text) {
+      return text?.description || "";
+    },
+    authors(author) {
+      return author?.authors || "";
+    },
   },
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.imgBorder {
+  border-radius: 15px;
+  border: 1px solid gray;
+}
+.imgPosition {
+  position: relative;
+}
+.textPosition {
+  position: absolute;
+  right: 0;
+  bottom: 55px;
+  border-radius: 1.5px;
+  padding: 1px 45px;
+  background-color: #1976d2;
+  color: white;
+}
+</style>
