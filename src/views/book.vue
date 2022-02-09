acc394a283b386507a0fcd96d9d9af457b02c50f
bexruz <bexruzbahronov2000@gmail.com>
2022-02-09 14:10:37 +0500

    fix book component design

--- src/views/book.vue
+++ src/views/book.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-container>
+    <v-container class="mx-auto">
       <v-row>
         <v-col cols="12" class="text-center" v-if="!bookInfo">
           <v-progress-circular
@@ -10,27 +10,33 @@
           ></v-progress-circular>
         </v-col>
         <v-col v-else cols="12" class="d-flex">
-          <span class="imgPosition mx-5">
-            <img
-              class="imgBorder"
-              :src="imgLinks(bookInfo.volumeInfo)"
-              alt=""
-            />
-            <p class="textPosition">#book</p>
-          </span>
-          <span class="d-block">
-            <span v-html="description(bookInfo.volumeInfo)"></span>
+          <v-row>
+            <v-col cols="12" md="6" class="mx-auto text-center">
+              <span class="imgPosition mx-5">
+                <img
+                  class="imgBorder"
+                  :src="imgLinks(bookInfo.volumeInfo)"
+                  alt=""
+                />
+                <p class="textPosition">#book</p>
+              </span>
+            </v-col>
+            <v-col cols="12" md="6" class="px-4">
+              <span class="d-block px-4">
+                <span v-html="description(bookInfo.volumeInfo)"></span>
 
-            <span class="d-flex py-3">
-              <p>Author:</p>
-              <span
-                v-for="author in authors(bookInfo.volumeInfo)"
-                :key="author.index"
-              >
-                <p class="px-2 primary--text">{{ author }};</p>
-              </span></span
-            >
-          </span>
+                <span class="d-flex py-3">
+                  <p>Author:</p>
+                  <span
+                    v-for="author in authors(bookInfo.volumeInfo)"
+                    :key="author.index"
+                  >
+                    <p class="px-2 primary--text">{{ author }};</p>
+                  </span></span
+                >
+              </span>
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
     </v-container>
@@ -66,12 +72,33 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.imgPosition {
+  position: relative;
+}
 .imgBorder {
+  width: 100%;
+  max-width: 450px;
   border-radius: 15px;
   border: 1px solid gray;
 }
-.imgPosition {
-  position: relative;
+
+@media only screen and (max-width: 768px) {
+  .imgBorder {
+    width: 100%;
+    max-width: 330px;
+  }
+}
+@media only screen and (max-width: 425px) {
+  .imgBorder {
+    width: 100%;
+    max-width: 275px;
+  }
 }
 .textPosition {
   position: absolute;
